c8b4ecf2187c173b43bf1ddc9b9adc3ac0a65a02
kush Muhamad <kushmohammed98@yahoo.com>
2023-03-28 21:07:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function isPalindrome(word) {
+function isPalindrome(str) {
   // Write your algorithm here
+  str = str.replace(/\W|_/g, "").toLowerCase(); // does not have underscore
+  let reversed = str.split("").reverse("").join("");
+  if (reversed === str) {
+    return true;
+  } else return false;
 }
-
+console.log(isPalindrome("abba"));
 /* 
   Add your pseudocode here
+  shall call a function called isPlaindrome
+  inside this function:
+  first remove any symbols or any special letters(/\W/g, "")
+  shall declare a variable called reverse 
+  which will split the argument(word) using the split function
+  and turn into an array
+  this will then be passed through a reverse method which reverse 
+  the letters in the array 
+  and finally shall use the method join to join the words 
+  if the value is the same as the word argument return true 
+  if not false
 */
 
 /*
